d9950d5d1dc9cf856bca6181287b46a0e0638862
Allwin <allwinluki28@gmail.com>
2022-04-05 14:03:46 +0700

    added some css changes for validity

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 const form = document.getElementById("form");
-const error = document.querySelector(".error");
+const passw = document.getElementById("pass");
+const conf = document.getElementById("confirm");
+const inputs = document.querySelectorAll(".inputs > input");
+
+conf.addEventListener("keyup", function(){
+    if(passw.value !== conf.value){
+        conf.style.border = "solid 2px red";
+        var tick = document.querySelector(".confirmpass");
+        tick.style.opacity = "0";
+    }else{
+        var tick = document.querySelector(".confirmpass");
+        tick.style.opacity = "1";
+        conf.style.border = "solid 2px green";
+    }
+})
+
+inputs.forEach(input => {
+    input.addEventListener('input', function(){
+        if(this.checkValidity()){
+            let name = this.getAttribute("name");
+            if(name === "confirmpass"){
+                if(passw.value === conf.value && this.checkValidity()){
+                    var tick = document.querySelector("." + name);
+                    tick.style.opacity = "1";
+                }
+            }else{
+            var tick = document.querySelector("." + name);
+            tick.style.opacity = "1";
+            }
+        };
+        if(!this.checkValidity()){
+            let name = this.getAttribute("name");
+            if(name === "confirmpass"){
+                if(passw.value !== conf.value || !this.checkValidity()){
+                    var tick = document.querySelector("." + name);
+                    tick.style.opacity = "0";
+                }
+            }
+            var tick = document.querySelector("." + name);
+            tick.style.opacity = "0";
+        }
+    });
+})
 
 form.addEventListener('submit', (e) => {
     let messages = [];
-    if(pass.value !== confirm.value){
+    if(passw.value !== conf.value){
         messages.push("Password confirmation doesn't match");
     }
 
     if(messages.length > 0){
     e.preventDefault();
-    error.innerText = messages.join(', ');
+    alert("Password confirmation doesn't match!");
     }
 });
